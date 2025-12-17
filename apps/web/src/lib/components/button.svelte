aad051ce0728628a633f9e0afb786111db6919c7
Anis Chahed <anis-chahed@cogent.co.jp>
2025-12-18 01:15:35 +0900

    feat(web): add save button

--- apps/web/src/lib/components/button.svelte
+++ apps/web/src/lib/components/button.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	let { children, onClick, position, type } = $props();
+	let { children, onClick, position, type, disabled = false } = $props();
 
 	let classes = $state('');
 	let colors = $state('');
@@ -9,12 +9,16 @@
 			classes = 'rounded-r-sm border ';
 		} else if (position === 'left') {
 			classes = 'rounded-l-sm border ';
-		} else {
+		} else if (position === 'middle') {
 			classes = 'border-t border-b';
+		} else {
+			classes = 'border rounded-sm';
 		}
 
 		if (type === 'danger') {
 			colors = `bg-red-600 hover:bg-red-700`;
+		} else if (type === 'save') {
+			colors = ` bg-green-100 hover:bg-green-300 border-green-600`;
 		} else {
 			colors = ` bg-gray-100 hover:bg-gray-300`;
 		}
@@ -22,6 +26,7 @@
 </script>
 
 <button
+	{disabled}
 	class={`w-12 h-10 flex items-center justify-center  cursor-pointer border-solid  border-gray-600 ${classes} ${colors} transition-colors`}
 	onclick={onClick}
 >
--- apps/web/src/routes/workflow/[id]/+page.svelte
+++ apps/web/src/routes/workflow/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from '$lib/components/button.svelte';
 	import Canvas from '$lib/components/canvas/canvas.svelte';
 	import ModalContainer from '$lib/components/modal/modal-container.svelte';
 	import Toolbox from '$lib/components/toolbox.svelte';
@@ -13,6 +14,7 @@
 	let canvasNodes: Map<string, CanvasNode> = $state(new Map());
 	let nodeFlowArrows: NodeFlowArrow[] = $state([]);
 	let selectedNode: CanvasNode | undefined = $state(undefined);
+	let shouldSave: boolean = $state(false);
 	let editNodeModal: any | undefined = $state.raw(undefined);
 
 	function createNewNode(canvasNode: CanvasNode, type: NodeType) {
@@ -76,14 +78,15 @@
 <svelte:window bind:innerWidth={width} bind:innerHeight={height} />
 
 <div id="workflow-container" class="relative h-full w-full">
-	<div
-		class={`${selectedNode ? 'visible' : 'hidden'} absolute w-full flex justify-center top-4 z-50`}
-	>
-		<Toolbox
-			onDeleteButtonPressed={deleteNode}
-			onEditButtonPressed={editNode}
-			onDuplicateButtonPressed={() => {}}
-		/>
+	<div class={` absolute w-full flex  top-4 left-4 z-50`}>
+		<Button
+			onClick={() => {}}
+			position="standalone"
+			type={shouldSave ? 'save' : 'normal'}
+			disabled={!shouldSave}
+			><span class={`pixelarticons--save  ${shouldSave ? 'text-green-600' : 'text-gray-600'}`}
+			></span></Button
+		>
 	</div>
 
 	<Canvas
@@ -97,4 +100,30 @@
 		onNodeSelected={selectNode}
 		onNodeMoved={moveNode}
 	/>
+
+	<div
+		class={`${selectedNode ? 'visible' : 'hidden'} absolute w-full flex justify-center bottom-10 z-50`}
+	>
+		<Toolbox
+			onDeleteButtonPressed={deleteNode}
+			onEditButtonPressed={editNode}
+			onDuplicateButtonPressed={() => {}}
+		/>
+	</div>
 </div>
+
+<style>
+	.pixelarticons--save {
+		display: inline-block;
+		width: 24px;
+		height: 24px;
+		--svg: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='%23000' d='M4 2h14v2H4v16h2v-6h12v6h2V6h2v16H2V2zm4 18h8v-4H8zM20 6h-2V4h2zM6 6h9v4H6z'/%3E%3C/svg%3E");
+		background-color: currentColor;
+		-webkit-mask-image: var(--svg);
+		mask-image: var(--svg);
+		-webkit-mask-repeat: no-repeat;
+		mask-repeat: no-repeat;
+		-webkit-mask-size: 100% 100%;
+		mask-size: 100% 100%;
+	}
+</style>
