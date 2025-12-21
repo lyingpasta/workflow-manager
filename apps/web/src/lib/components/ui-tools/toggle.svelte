dd534e365828858404df572e724a1a3e59382ca6
Anis Chahed <anis-chahed@cogent.co.jp>
2025-12-22 00:08:12 +0900

    feat(web): better toggle

--- apps/web/src/lib/components/ui-tools/toggle.svelte
+++ apps/web/src/lib/components/ui-tools/toggle.svelte
@@ -6,7 +6,7 @@
 
 <button
 	title="activation"
-	class={`h-6 w-12 cursor-pointer rounded-xs border border-solid p-0.5 ${toggled ? 'left-1/2 border-blue-500 bg-amber-500 ' : 'border-gray-400 bg-gray-100'}`}
+	class={`h-6 w-12 cursor-pointer rounded-xs border border-solid p-0.5 ${toggled ? 'left-1/2 border-blue-500 bg-blue-200 ' : 'border-gray-400 bg-gray-50'}`}
 	onclick={(event) => {
 		event.stopPropagation();
 		onToggle();
@@ -15,6 +15,6 @@
 	onmouseleave={() => (hovered = false)}
 >
 	<div
-		class={`relative h-full w-6 rounded-xs transition-all ${hovered && !toggled ? 'translate-x-0.5' : ''} ${toggled ? 'translate-x-4.5 bg-blue-500' : 'bg-gray-400'}`}
+		class={`relative h-full w-6 rounded-xs transition-all ${hovered && !toggled ? 'translate-x-0.5' : ''} ${toggled ? 'translate-x-4.5 bg-blue-400' : 'bg-gray-400'}`}
 	></div>
 </button>
