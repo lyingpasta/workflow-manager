6913579baafb5255dc623809f1e00f410994ddae
Anis Chahed <anis-chahed@cogent.co.jp>
2025-12-21 19:10:19 +0900

    feat(core): add get workflow schema

--- apps/core/src/infrastructure/controllers/workflow-schema.controller.ts
+++ apps/core/src/infrastructure/controllers/workflow-schema.controller.ts
@@ -1,16 +1,29 @@
-import { Body, Controller, Inject, Patch } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Inject, Patch, Query } from '@nestjs/common';
 import { SaveWorkflowSchemaUseCase } from 'src/domain/use-case/save-workflow-schema.use-case';
 import { SaveWorkflowSchemaDTO } from './dto/save-workflow-schema.dto';
+import { GetWorkflowSchemaUseCase } from 'src/domain/use-case/get-workflow-schema.use-case';
+import { match, P } from 'ts-pattern';
 
 @Controller('/schemas')
 export class WorkflowSchemaController {
   constructor(
     @Inject()
-    private readonly saveWorkflowSchemaUseCase: SaveWorkflowSchemaUseCase
+    private readonly saveWorkflowSchemaUseCase: SaveWorkflowSchemaUseCase,
+    @Inject()
+    private readonly getWorkflowSchemaUseCase: GetWorkflowSchemaUseCase
   ) { }
 
   @Patch()
-  createWorkflow(@Body() input: SaveWorkflowSchemaDTO) {
+  saveWorkflowSchema(@Body() input: SaveWorkflowSchemaDTO) {
     return this.saveWorkflowSchemaUseCase.execute(input);
+  }
+
+  @Get()
+  getWorkflowSchemaByWorkflowId(@Query() query: { workflowId?: string }) {
+    return match(query)
+      .with({ workflowId: P.string.select() }, (id) => this.getWorkflowSchemaUseCase.execute(id))
+      .otherwise(() => {
+        throw new BadRequestException("unknown query params")
+      })
   }
 }
